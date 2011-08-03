30b8a1286d4d46163c4d9d864967673c2b423558
Wojciech Todryk <wojciech@todryk.pl>
2011-08-03 19:06:27 +0200

    skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarnivoreNew::Application.config.secret_token = '3924e08667b0529b91e2ac90d9f89020ab26155ab98938a249defd0bd4b1c6b6549e1d05f0360b4579c23661e82232a076ceb386875036651bf98709ef046a19'
