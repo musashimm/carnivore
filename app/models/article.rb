27060143972f44ab40de026133ee20493b8722ca
Wojciech Todryk <wojciech@todryk.pl>
2011-08-06 15:51:18 +0200

    switch off comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,16 @@
 class Article < ActiveRecord::Base
 	belongs_to :category
-	has_many :comments, :dependent => :destroy 
+	has_many :comments, :dependent => :destroy
 	validates_presence_of :title,:body,:category_id
 	#validates_uniqueness_of :title
 	#before_save :generate_permalink, :only => 'create'
-	
+
 	# 	protected
 	# 	def generate_permalink
 	# 		self.permalink = title.downcase.gsub(/\W/,'-').gsub(/-+/,'-')
 	# 	end
+
+	def qwerty
+		false
+	end
 end
